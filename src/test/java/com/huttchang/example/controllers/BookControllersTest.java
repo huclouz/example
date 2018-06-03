bdb94322ce750407a3524d4ab3830faf58a93490
신휴창 <huttchang@gmail.com>
2018-06-04 03:37:45 +0900

    테스트케이스 추가

--- src/test/java/com/huttchang/example/controllers/BookControllersTest.java
+++ src/test/java/com/huttchang/example/controllers/BookControllersTest.java
@@ -1,17 +1,86 @@
 package com.huttchang.example.controllers;
 
-import static org.junit.Assert.*;
+import com.huttchang.example.models.Book;
+import com.huttchang.example.models.BookMark;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.ui.ConcurrentModel;
+import org.springframework.ui.Model;
 
-/**
- * 해당 파일은 소유권은 신휴창에게 있습니다.
- * 현재 오픈 소스로 공개중인 버전은 AGPL을 따르는 오픈 소스 프로젝트이며,
- * 소스 코드를 수정하셔서 사용하는 경우에는 반드시 동일한 라이센스로 소스 코드를 공개하여야 합니다.
- * 만약 HUCLOUD를 상업적으로 이용하실 경우에는 라이센스를 구매하여 사용하셔야 합니다.
- * email : huttchang@gmail.com
- * 프로젝트명    : example
- * 작성 및 소유자 : hucloud
- * 최초 생성일   : 2018. 5. 28.
- */
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class BookControllersTest {
+
+    @Autowired
+    private BookController bookController;
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testBookList() throws Exception {
+        ResponseEntity<Model> response = bookController.getBookList("title", "사랑", "50", "1", new ConcurrentModel(), "eyJzZWMiOiJ1cml0eSIsImFsZyI6IkhTMjU2In0.eyJqd3QiOnsiaWQiOjE1LCJlbWFpbCI6Imh1dHRjaGFuZ0BnbWFpbC5jb20iLCJuYW1lIjoi7Iug7Zy07LC9IiwicGFzc3dvcmQiOiIiLCJzdGF0dXMiOjIsImNyZWF0ZURhdGUiOm51bGwsInRva2VuIjpudWxsfX0.P2DqMGCzkpmtTVysZP0e7hRoVuaTlM1Ui9Ur4k97Bs4");
+        List<Book> searchData = (List<Book>) response.getBody().asMap().get("data");
+        assertNotNull(searchData);
+        System.out.println(String.format("%s : Test Result = %s", "[testBookList]", searchData.toString()));
+
+    }
+
+    @Test
+    public void testBookDetail() throws Exception {
+        List<Book> books = bookController.getBookDetail("8960773425");
+        assertNotNull(books);
+        System.out.println(String.format("%s : Test Result = %s", "[testBookDetail]", books.toString()));
+    }
+
+    @Test
+    public void testBookmarkList() throws Exception {
+        List<BookMark> data = bookController.getBookMarks("eyJzZWMiOiJ1cml0eSIsImFsZyI6IkhTMjU2In0.eyJqd3QiOnsiaWQiOjE1LCJlbWFpbCI6Imh1dHRjaGFuZ0BnbWFpbC5jb20iLCJuYW1lIjoi7Iug7Zy07LC9IiwicGFzc3dvcmQiOiIiLCJzdGF0dXMiOjIsImNyZWF0ZURhdGUiOm51bGwsInRva2VuIjpudWxsfX0.P2DqMGCzkpmtTVysZP0e7hRoVuaTlM1Ui9Ur4k97Bs4");
+        assertNotNull(data);
+        System.out.println(String.format("%s : Test Result = %s", "[testBookmarkList]", data.toString()));
+    }
+
+    @Test
+    public void testBookmark() throws Exception {
+        try{
+            BookMark mark = new BookMark();
+            mark.setUserId(15);
+            mark.setBookIsbn("8960773425");
+            bookController.addBookMarks(mark, "eyJzZWMiOiJ1cml0eSIsImFsZyI6IkhTMjU2In0.eyJqd3QiOnsiaWQiOjE1LCJlbWFpbCI6Imh1dHRjaGFuZ0BnbWFpbC5jb20iLCJuYW1lIjoi7Iug7Zy07LC9IiwicGFzc3dvcmQiOiIiLCJzdGF0dXMiOjIsImNyZWF0ZURhdGUiOm51bGwsInRva2VuIjpudWxsfX0.P2DqMGCzkpmtTVysZP0e7hRoVuaTlM1Ui9Ur4k97Bs4");
+            System.out.println(String.format("%s : Test Result = %s", "[testBookmark]", "SUCCESS"));
+        }catch (Exception e){
+            System.out.println(String.format("%s : Test Result = %s", "[testBookmark]", "FAILED"));
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testDeleteBookmark() throws Exception {
+        try{
+            bookController.delBookMarks(1);
+            System.out.println(String.format("%s : Test Result = %s", "[testDeleteBookmark]", "SUCCESS"));
+        }catch (Exception e){
+            System.out.println(String.format("%s : Test Result = %s", "[testDeleteBookmark]", "Fail"));
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testHistoryList() throws Exception {
+        List<Book> data = bookController.getHistoryList("eyJzZWMiOiJ1cml0eSIsImFsZyI6IkhTMjU2In0.eyJqd3QiOnsiaWQiOjE1LCJlbWFpbCI6Imh1dHRjaGFuZ0BnbWFpbC5jb20iLCJuYW1lIjoi7Iug7Zy07LC9IiwicGFzc3dvcmQiOiIiLCJzdGF0dXMiOjIsImNyZWF0ZURhdGUiOm51bGwsInRva2VuIjpudWxsfX0.P2DqMGCzkpmtTVysZP0e7hRoVuaTlM1Ui9Ur4k97Bs4");
+        assertNotNull(data);
+        System.out.println(String.format("%s : Test Result = %s", "[testHistoryList]", data.toString()));
+    }
 
 }
